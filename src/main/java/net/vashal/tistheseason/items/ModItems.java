25f6921c873ed36d3c2927f309788ff935df74ec
Aaron Li <aaronli@Aarons-MacBook-Pro.local>
2022-11-18 15:54:24 -0600

    refactor to use constants

--- src/main/java/net/vashal/tistheseason/items/ModItems.java
+++ src/main/java/net/vashal/tistheseason/items/ModItems.java
@@ -6,14 +6,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.vashal.tistheseason.TisTheSeason;
+import net.vashal.tistheseason.entity.ToyRobotConstants;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TisTheSeason.MOD_ID);
 
-    public static final RegistryObject<Item> TOYROBOT = ITEMS.register("toyrobot",
+    public static final RegistryObject<Item> TOYROBOT = ITEMS.register(ToyRobotConstants.NAME,
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TISTHESEASON_TAB)));
-
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
