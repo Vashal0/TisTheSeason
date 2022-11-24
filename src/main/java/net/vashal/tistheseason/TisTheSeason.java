24dafac613648a2bb868a7f39a249d81106a0702
Vashal <arthurjreed1@gmail.com>
2022-11-24 15:09:00 -0500

    made progress on adding functionality to the table

--- src/main/java/net/vashal/tistheseason/TisTheSeason.java
+++ src/main/java/net/vashal/tistheseason/TisTheSeason.java
@@ -1,13 +1,9 @@
 package net.vashal.tistheseason;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,20 +12,19 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.ObjectHolder;
 import net.vashal.tistheseason.block.TTS_Blocks;
 import net.vashal.tistheseason.block.entity.TTS_BlockEntities;
 import net.vashal.tistheseason.entity.TTS_EntityTypes;
-import net.vashal.tistheseason.entity.client.*;
+import net.vashal.tistheseason.entity.client.ToyRobotRenderer;
+import net.vashal.tistheseason.entity.client.ToySoldierRenderer;
 import net.vashal.tistheseason.items.TTS_Items;
-import net.vashal.tistheseason.items.custom.HobbyHorseItem;
+import net.vashal.tistheseason.screen.TTS_MenuTypes;
+import net.vashal.tistheseason.screen.ToyWorkbenchScreen;
 import net.vashal.tistheseason.sounds.TTS_Sounds;
 import org.slf4j.Logger;
 import software.bernie.geckolib3.GeckoLib;
 import top.theillusivec4.curios.api.SlotTypeMessage;
 import top.theillusivec4.curios.api.SlotTypePreset;
-import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TisTheSeason.MOD_ID)
@@ -54,6 +49,7 @@ public class TisTheSeason {
         TTS_EntityTypes.register(modEventBus);
         TTS_Sounds.SOUNDS.register(modEventBus);
         TTS_BlockEntities.register(modEventBus);
+        TTS_MenuTypes.register(modEventBus);
     }
 
 
@@ -69,6 +65,8 @@ public class TisTheSeason {
 
             EntityRenderers.register(TTS_EntityTypes.TOYROBOT.get(), ToyRobotRenderer::new);
             EntityRenderers.register(TTS_EntityTypes.TOYSOLDIER.get(), ToySoldierRenderer::new);
+
+            MenuScreens.register(TTS_MenuTypes.TOY_WORKBENCH_MENU.get(), ToyWorkbenchScreen::new);
         }
     }
 }
--- src/main/java/net/vashal/tistheseason/block/entity/ToyWorkbenchBlockEntity.java
+++ src/main/java/net/vashal/tistheseason/block/entity/ToyWorkbenchBlockEntity.java
@@ -12,6 +12,9 @@ import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -20,11 +23,13 @@ import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
+import net.vashal.tistheseason.items.TTS_Items;
+import net.vashal.tistheseason.screen.ToyWorkbenchMenu;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ToyWorkbenchBlockEntity extends BlockEntity implements MenuProvider {
-    private final ItemStackHandler itemHandler =  new ItemStackHandler(3) {
+    private final ItemStackHandler itemHandler =  new ItemStackHandler(4) {
         @Override
         protected void onContentsChanged(int slot) {
             setChanged();
@@ -33,8 +38,36 @@ public class ToyWorkbenchBlockEntity extends BlockEntity implements MenuProvider
 
     private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
 
+    protected final ContainerData data;
+    private int progress = 0;
+    private int maxProgress = 78;
+
     public ToyWorkbenchBlockEntity(BlockPos pos, BlockState state) {
         super(TTS_BlockEntities.TOY_WORKBENCH.get(), pos, state);
+        this.data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return switch (index) {
+                    case 0 -> ToyWorkbenchBlockEntity.this.progress;
+                    case 1 -> ToyWorkbenchBlockEntity.this.maxProgress;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch (index) {
+                    case 0 -> ToyWorkbenchBlockEntity.this.progress = value;
+                    case 1 -> ToyWorkbenchBlockEntity.this.maxProgress = value;
+                }
+
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
     }
 
     @Override
@@ -45,7 +78,7 @@ public class ToyWorkbenchBlockEntity extends BlockEntity implements MenuProvider
     @Nullable
     @Override
     public AbstractContainerMenu createMenu(int id, Inventory playerInventory, Player player) {
-        return null;
+        return new ToyWorkbenchMenu(id, playerInventory, this, this.data);
     }
 
 
@@ -91,9 +124,56 @@ public class ToyWorkbenchBlockEntity extends BlockEntity implements MenuProvider
         Containers.dropContents(this.level, this.worldPosition, inventory);
     }
 
-    public static <E extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState state, E e) {
+    public static <E extends BlockEntity> void tick(Level level, BlockPos blockPos, BlockState state, ToyWorkbenchBlockEntity entity) {
         if(level.isClientSide) {
             return;
         }
+
+        if(hasRecipe(entity)) {
+            entity.progress++;
+            setChanged(level, blockPos, state);
+
+            if(entity.progress >= entity.maxProgress) {
+                craftItem(entity);
+            }
+        } else {
+            entity.resetProgress();
+            setChanged(level, blockPos, state);
+        }
+    }
+
+    private void resetProgress() {
+        this.progress = 0;
+    }
+
+    private static void craftItem(ToyWorkbenchBlockEntity entity) {
+
+        if(hasRecipe(entity)) {
+            entity.itemHandler.extractItem(1,1,false);
+            entity.itemHandler.setStackInSlot(2, new ItemStack(TTS_Items.TOYROBOT.get(),
+                    entity.itemHandler.getStackInSlot(2).getCount() + 1));
+
+            entity.resetProgress();
+        }
+    }
+
+    private static boolean hasRecipe(ToyWorkbenchBlockEntity entity) {
+        SimpleContainer inventory = new SimpleContainer(entity.itemHandler.getSlots());
+        for (int i = 0; i < entity.itemHandler.getSlots(); i++) {
+            inventory.setItem(i, entity.itemHandler.getStackInSlot(i));
+        }
+
+        boolean hasDyeInFirstSlot = entity.itemHandler.getStackInSlot(1).getItem() == Items.YELLOW_DYE;
+
+        return hasDyeInFirstSlot && canInsertAmountIntoOutputSlot(inventory) &&
+                canInsertItemIntoOutputSlot(inventory, new ItemStack(Items.YELLOW_DYE, 1));
+    }
+
+    private static boolean canInsertItemIntoOutputSlot(SimpleContainer inventory, ItemStack itemStack) {
+        return inventory.getItem(2).getItem() == itemStack.getItem() || inventory.getItem(2).isEmpty();
+    }
+
+    private static boolean canInsertAmountIntoOutputSlot(SimpleContainer inventory) {
+        return inventory.getItem(2).getMaxStackSize() > inventory.getItem(2).getCount();
     }
 }
